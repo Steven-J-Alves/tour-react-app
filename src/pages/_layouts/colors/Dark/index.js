15306e4520b0a82a41363db1645b357c3da314f0
Steven <stevenalves512@gmail.com>
2021-04-03 21:24:39 -0100

    TR-10: Global Styles and Colors

---
+++ src/pages/_layouts/colors/Dark/index.js
@@ -0,0 +1,11 @@
+/* Buttons colors */
+export const DefaultButton = '#e3e3e3';
+export const PrimaryButton = '#F89033';
+export const DangerButton = '#EA5858';
+export const ConfirmButton = '#73D888';
+
+/* Buttons text colors */
+export const DefaultTextColor = '#34495e';
+export const PrimaryTextColor = '#34495e';
+export const DangerTextColor = '#34495e';
+export const ConfirmTextColor = '#34495e';
