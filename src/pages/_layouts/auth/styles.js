0b0d7b016bbcf145e3a7a1972ebaa779aba68c8e
Steven <stevenalves512@gmail.com>
2021-04-11 15:01:36 -0100

    TR-16: Fix Routes, GLobal Styles and Remove Layout

--- src/pages/_layouts/auth/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  height: 100%;
-  background: linear-gradient(-90deg, #29bb89, #1597bb);
-`;
--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -2,49 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
-
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = true;
-
-  const { path } = rest;
+  const signed = false;
 
   if (!signed && isPrivate) {
     return <Redirect to="/signin" />;
   }
 
-  if (signed && path === '/signin') {
-    return <Redirect to="/" />;
-  }
-
-  if (signed && path === '/signup') {
-    return <Redirect to="/" />;
-  }
-
-  // AuthLayout -> no login; display signin and signup form
-  // DefaultLayout -> login display component
-  let Layout = signed ? DefaultLayout : AuthLayout;
-
-  if (path === '/' || path === '/tour/:id') {
-    Layout = DefaultLayout;
-  }
-
-  return (
-    <Route
-      {...rest}
-      // function that receive all props (navigation, route params, history...)
-      render={(props) => (
-        <Layout>
-          <Component {...props} />
-        </Layout>
-      )}
-    />
-  );
+  return <Route {...rest} component={Component} />;
 }
 
 RouteWrapper.propTypes = {
