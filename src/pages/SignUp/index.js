d144229083ab6b5405166107f14f4d9e441a74f9
Steven <stevenalves512@gmail.com>
2021-04-13 22:42:32 -0100

    TR-22: Create and Style SignIn and SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Content } from './styles';
 
 function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <Container>
+      <Content>
+        <form>
+          <h1>Create your account!</h1>
+          <div>
+            <label htmlFor="name">Your name</label>
+            <input type="text" placeholder="your name" name="name" required />
+          </div>
+          <div>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="text"
+              placeholder="your@example.com"
+              name="email"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              name="password"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="confirm_password">Confirm Password</label>
+            <input
+              type="password"
+              placeholder="••••••••"
+              name="confirm_password"
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">SIGN UP</button>
+          </div>
+          <Link to="/signin">Have a account?</Link>
+        </form>
+      </Content>
+    </Container>
+  );
 }
 
 export default SignUp;
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -11,11 +11,17 @@ export default createGlobalStyle`
   *:focus {
     outline: 0;
   }
+  html {
+    font-size: 62.5%;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
   html, body, #root {
     height: 100%;
   }
   body {
     -webkit-font-smoothing: antialiased;
+    color: #777;
   }
   body, input, button {
     font: 14px 'Roboto', sans-serif;
