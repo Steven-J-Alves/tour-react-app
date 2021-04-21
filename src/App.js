b99d9bc9121f508fca768628b462d9b7c75f5bd2
Steven Alves <stevenalves512@gmail.com>
2021-04-21 17:08:47 -0100

    TR-28: Store User Profile and Persist Auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store';
+import { store, persistStore_ } from './store';
 
 import GlobalStyles from './styles/globalStyles';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyles />
-      </Router>
+      <PersistGate persistor={persistStore_}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyles />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import createSagaMiddleware from 'redux-saga';
+import { persistStore } from 'redux-persist';
+
 import createStore from './createStore';
+
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
+
+import persistReducers from './persistReducers';
 
 const SagaMiddleware = createSagaMiddleware();
 
 const middlewares = [SagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistReducers(rootReducer), middlewares);
+const persistStore_ = persistStore(store);
 
 SagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistStore_ };
