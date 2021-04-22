a81e33b227abb31273794a35b87b1b1f240ae2bb
Steven Alves <stevenalves512@gmail.com>
2021-04-22 08:35:47 -0100

    TR-30: Request Auth

--- src/pages/Overview/index.js
+++ src/pages/Overview/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 function Overview() {
+  api.get('users/me');
+
   return <h1>Overview</h1>;
 }
 
